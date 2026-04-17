95a4a95e6859f3addd31859d97906913e70c96c8
granth-alpha2 <chauhangranth02@gmail.com>
2026-04-17 14:21:41 +0530

    Added test step to CI/CD pipeline

--- index.js
+++ index.js
@@ -2,7 +2,11 @@ const express = require('express');
 const app = express();
 
 app.get('/health', (req, res) => {
-    res.json({ status: 'Server running' });
+  res.json({ status: 'Server running' });
 });
 
-app.listen(3000);
+module.exports = app;
+
+if (require.main === module) {
+  app.listen(3000);
+}
